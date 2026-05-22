516f215e73f23b24a7ec9549a532faa826f38119
Fernando Dev <ferreirareiscabralfernando@gmail.com>
2026-05-22 20:30:26 -0300

    Iniciando a VM.

--- src/app.d
+++ src/app.d
@@ -1,8 +1,23 @@
 module app;
 
 import harpyvm;
+import utils;
 
 extern(C) int main(int argc, char** args) 
 {
+    Arena arena = Arena(64);
+    scope (exit) arena.destruct();
+    GLOBAL_ARENA = &arena;
+
+    uint len;
+    uint* program = cast(uint*) arena.alloc(24);
+    program[len++] = cast(uint)(HarpyOpCode.Halt);
+
+    HarpyVirtualMachine hVirtualMachine = HarpyVirtualMachine(program, len);
+    scope (exit) hVirtualMachine.destruct();
+    
+    hVirtualMachine.setup();
+    hVirtualMachine.run();
+
     return 0;
 }
--- src/harpyvm.d
+++ src/harpyvm.d
@@ -1,7 +1,101 @@
-// Harpy Virutal Machine - Baseada em registradores, leve e otimizada
-// muito inspirada pela VM de Lua e pela JVM, o objetivo é se tornar backend de absolutamente qualquer linguagem de programação interpretada
-// com um sistema de tipos extremamente rico suportando estruturas de dados complexas nativamente como arrays, hashtables e objects
-// Fernando © 2025
-
 module harpyvm;
 
+import utils;
+
+alias Program = uint*;
+
+enum HarpyOpCode : ubyte {
+    Load,
+    Store,
+    Add,
+    Sub,
+    Halt,
+}
+
+enum HarpyValueType : ubyte {
+    Int,
+    Float,
+    Double,
+    Bool,
+}
+
+union HarpyValueData {
+    long l;
+    double d;
+    float f;
+}
+
+struct HarpyValue {
+    HarpyValueType type; // 1
+    HarpyValueData value; // 8
+}
+
+struct HarpyFrameCall {
+    uint dpc;
+    uint pc;
+}
+
+enum DATA_SIZE_INIT = 16;
+enum DATA_SIZE_LIMIT = 1_000_000;
+
+struct HarpyVirtualMachine
+{
+private:
+    Program program;
+    uint psize;
+    uint pc;
+
+    HarpyValue* data;
+    uint dsize;
+    uint dcap;
+
+public:
+    pragma(inline, true)
+    void setup()
+    {
+        data = cast(HarpyValue*) calloc(1, HarpyValue.sizeof * DATA_SIZE_INIT);
+        hvmAssert(data !is null, "erro ao alocar memoria pro data.");
+        
+        dcap = DATA_SIZE_INIT;
+        for (uint i; i < dcap; i++)
+            data[i] = HarpyValue.init; // inicializa todos os slots
+    }
+
+    pragma(inline, true)
+    void resize(uint needed)
+    {
+        // se uma função precisa de N registradores e (dsize + N) > dcap
+        // logo precisamos fazer um resize dos dados alocando mais memoria
+        if ((dsize + needed) > dcap) return;
+
+        uint cap = dcap;
+        dcap = (dsize + needed) * 2; // poderia só dobrar a capacidade, mas vamos usar algo menos fixo baseado nas necessidades
+
+        hvmAssert(dcap < DATA_SIZE_LIMIT, "quantidade máxima de dados alocados.");
+
+        HarpyValue* newData = cast(HarpyValue*) calloc(1, HarpyValue.sizeof * dcap);
+        hvmAssert(newData !is null, "erro ao alocar memoria pro newData.");
+        
+        for (uint i; i < cap; i++)
+            newData[i] = data[i]; // reinsere os dados
+        
+        for (uint i = cap; i < dcap; i++)
+            newData[i] = HarpyValue.init; // inicializa os outros slots
+
+        free(data); // libera a memoria antiga
+        data = newData; // atualiza
+    }
+
+    pragma(inline, true)
+    void run()
+    {
+        //
+    }
+
+    pragma(inline, true)
+    void destruct()
+    {
+        if (!data) return;
+        free(data);
+    }
+}
